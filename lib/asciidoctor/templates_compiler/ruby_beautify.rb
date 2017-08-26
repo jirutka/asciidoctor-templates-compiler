2a9e402d523fe2efdc1b10e21ba6def1bb01dddf
Jakub Jirutka <jakub@jirutka.cz>
2017-08-27 01:37:16 +0200

    RubyBeautify: Patch to fix failure when "ruby" is not on PATH
    For example when running on JRuby w/o MRI ruby installed.

--- lib/asciidoctor/templates_compiler/ruby_beautify.rb
+++ lib/asciidoctor/templates_compiler/ruby_beautify.rb
@@ -3,26 +3,44 @@ require 'ruby-beautify'
 
 module Asciidoctor::TemplatesCompiler
   module RubyBeautify
+    include ::RubyBeautify
     extend self
 
+    alias_method :pretty_string_orig, :pretty_string
+    private :pretty_string_orig
+
     def pretty_string(code, indent_count: 2)
-      new_lines_old = ::RubyBeautify::NEW_LINES
-      ::RubyBeautify::NEW_LINES.push(:on_semicolon)  # XXX: sandbox somehow?
+      new_lines_old = NEW_LINES
+      NEW_LINES.push(:on_semicolon)  # XXX: sandbox somehow?
 
       s = "module M_\n#{code}\nend\n"
       s.gsub! /^[ \t]*;/, ''      # remove leading semicolons
       s.gsub! /;\s*$/, ''         # remove trailing semicolons
-      s.replace ::RubyBeautify.pretty_string(s, indent_token: "\1", indent_count: indent_count)
+      s.replace pretty_string_orig(s, indent_token: "\1", indent_count: indent_count)
       s.gsub! ";\1", "\n\1"       # remove trailing semicolons after formatting
       s.gsub! /^#{"\1" * indent_count}/, ''  # remove redundant indentation level
       s.gsub! "\1", ' '           # replace placeholder indent char with space
       s.sub! /\Amodule M_\n/, ''  # remove wrapper module
       s.sub! /\nend\n\z/, ''      # remove wrapper module
 
-      ::RubyBeautify::NEW_LINES.replace(new_lines_old)  # XXX: not thread-safe
+      NEW_LINES.replace(new_lines_old)  # XXX: not thread-safe
       s
     end
 
-    alias :call :pretty_string
+    alias_method :call, :pretty_string
+
+    # XXX: Remove after https://github.com/erniebrodeur/ruby-beautify/pull/43 is merged.
+    # Overwrite this method from ::RubyBeautify with implementation that does
+    # not execute ruby subprocess.
+    def syntax_ok?(string)
+      begin
+        catch :good do
+          eval "BEGIN { throw :good }; #{string}"
+        end
+        true
+      rescue SyntaxError
+        false
+      end
+    end
   end
 end
