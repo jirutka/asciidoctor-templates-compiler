10c0b38b7454fd868bfd2a8f9bb4d3b52645f8bf
Jakub Jirutka <jakub@jirutka.cz>
2017-08-27 16:12:41 +0200

    Report test coverage to Codacy

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,13 @@ RSpec.configure do |config|
   config.color = true
 end
 
+formatters = [SimpleCov::Formatter::HTMLFormatter]
+if ENV['CODACY_PROJECT_TOKEN']
+  require 'codacy-coverage'
+  formatters << Codacy::Formatter
+end
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new(formatters)
 SimpleCov.start do
   add_filter '/spec/'
 end
