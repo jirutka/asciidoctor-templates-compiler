c80d9ac2c2eea14e5a372372b43c62c837ce1fe9
Jakub Jirutka <jakub@jirutka.cz>
2017-08-27 18:08:08 +0200

    Do not run SimpleCov on JRuby
    It reports 0 %, JRuby probably needs to run in debug mode or something
    to get this work.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,20 +1,23 @@
 require 'rspec'
-require 'simplecov'
 require_relative 'support/matchers'
 
 RSpec.configure do |config|
   config.color = true
 end
 
-formatters = [SimpleCov::Formatter::HTMLFormatter]
-if ENV['CODACY_PROJECT_TOKEN']
-  require 'codacy-coverage'
-  formatters << Codacy::Formatter
-end
+unless RUBY_ENGINE == 'jruby'
+  require 'simplecov'
 
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new(formatters)
-SimpleCov.start do
-  add_filter '/spec/'
+  formatters = [SimpleCov::Formatter::HTMLFormatter]
+  if ENV['CODACY_PROJECT_TOKEN']
+    require 'codacy-coverage'
+    formatters << Codacy::Formatter
+  end
+
+  SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new(formatters)
+  SimpleCov.start do
+    add_filter '/spec/'
+  end
 end
 
 require 'asciidoctor-templates-compiler'
